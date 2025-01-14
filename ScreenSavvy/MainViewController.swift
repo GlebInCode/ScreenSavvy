56d2ffb61fa69acd55d2fefa4b96751b67fa4888
Khamin Gleb <nimik19@yandex.ru>
2025-01-14 09:44:21 +0300

    Initial commit

---
+++ ScreenSavvy/MainViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MainViewController.swift
+//  ScreenSavvy
+//
+//  Created by Глеб Хамин on 14.01.2025.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
+
